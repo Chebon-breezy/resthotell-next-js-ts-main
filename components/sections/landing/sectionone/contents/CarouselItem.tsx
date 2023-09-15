6a8e68580a7908463362ac60e90649d47f6da6ba
Brian Chebon <brynnlamura@gmail.com>
2023-09-15 17:34:54 +0300

    made currosel to swip/slide

--- components/sections/landing/sectionone/contents/CarouselItem.tsx
+++ components/sections/landing/sectionone/contents/CarouselItem.tsx
@@ -27,43 +27,85 @@ const CarouselItem = () => {
   const { Grid, Left, Right } = CarouselItem;
   return (
     <div className="">
-      <Grid>
-        <Left>
-          <div className="flex gap-x-2">
-            <Image
-              src={
-                "https://restho-nextjs.vercel.app/assets/images/icon/sub-title-vec.svg"
-              }
-              alt=""
-              width={20}
-              height={20}
-            />
-            <Paragraph className="text-[#bf9444]">Welcome to Restho</Paragraph>
-            <Image
-              src={
-                "https://restho-nextjs.vercel.app/assets/images/icon/sub-title-vec.svg"
-              }
-              alt=""
-              width={20}
-              height={20}
-            />
-          </div>
-          <Heading className="text-xl md:text-7xl leading-10 ">
-            Find Your Best Health and Tasty Food
-          </Heading>
-          <Paragraph className="text-base md:text-xl">
-            It is long extablished fact that a reader will be distracted by the
-            readable content of the page
-          </Paragraph>
-          <Button className="flex items-center text-[#bf9444] px-6 py-4 gap-x-3 border border-[#bf9444] rounded-full max-w-xs justify-center">
-            <BsArrowUpRightCircle />
-            <Paragraph>Discover More</Paragraph>
-          </Button>
-        </Left>
-        <Right>
-          <KiteLayout />
-        </Right>
-      </Grid>
+      <Swiper
+        direction="horizontal"
+        speed={6000}
+        slidesPerView={1}
+        centeredSlides={false}
+        grabCursor={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        keyboard={{
+          enabled: true,
+        }}
+        scrollbar={false}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        pagination={{
+          enabled: false,
+          el: ".swiper-pagination",
+          clickable: true,
+        }}
+        modules={[
+          Keyboard,
+          Scrollbar,
+          Navigation,
+          Pagination,
+          Autoplay,
+          Mousewheel,
+        ]}
+        className="mySwiper"
+      >
+        {Array(7)
+          .fill("")
+          .map((i) => (
+            <SwiperSlide key={i} style={{ width: "100%" }}>
+              <Grid>
+                <Left>
+                  <div className="flex gap-x-2">
+                    <Image
+                      src={
+                        "https://restho-nextjs.vercel.app/assets/images/icon/sub-title-vec.svg"
+                      }
+                      alt=""
+                      width={20}
+                      height={20}
+                    />
+                    <Paragraph className="text-[#bf9444]">
+                      Welcome to Restho
+                    </Paragraph>
+                    <Image
+                      src={
+                        "https://restho-nextjs.vercel.app/assets/images/icon/sub-title-vec.svg"
+                      }
+                      alt=""
+                      width={20}
+                      height={20}
+                    />
+                  </div>
+                  <Heading className="text-xl md:text-7xl leading-10 ">
+                    Find Your Best Health and Tasty Food
+                  </Heading>
+                  <Paragraph className="text-base md:text-xl">
+                    It is long extablished fact that a reader will be distracted
+                    by the readable content of the page
+                  </Paragraph>
+                  <Button className="flex items-center text-[#bf9444] px-6 py-4 gap-x-3 border border-[#bf9444] rounded-full max-w-xs justify-center">
+                    <BsArrowUpRightCircle />
+                    <Paragraph>Discover More</Paragraph>
+                  </Button>
+                </Left>
+                <Right>
+                  <KiteLayout />
+                </Right>
+              </Grid>
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </div>
   );
 };
