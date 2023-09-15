faa37fc904533554bb1c148cc221d99a01205cf8
Brian Chebon <brynnlamura@gmail.com>
2023-09-15 17:10:38 +0300

    added the nav-top

--- components/sections/navbar/contents/NavbarTop.tsx
+++ components/sections/navbar/contents/NavbarTop.tsx
@@ -3,34 +3,44 @@ import PageWrapper from "@/components/layout/PageWrapper";
 import React from "react";
 import tw from "tailwind-styled-components";
 import { HiOutlineMail, HiOutlineLocationMarker } from "react-icons/hi";
+
 const NavbarTop = () => {
   const NavbarTopStyles = {
-    Layout: tw.div`flex justify-between px-20 w-full py-5`,
-    Flex: tw.div`md:flex items-center text-xl font-medium gap-x-2 text-[#f8f9fa]`,
+    Layout: tw.div`flex justify-start items-center px-4 md:px-10 w-full py-2 md:py-5 bg-[#1A1C14] text-sm md:text-base`,
+    Flex: tw.div`flex items-center gap-x-2 text-[#f8f9fa]`,
+    Email: tw.div`flex items-left gap-x-1`,
+    Location: tw.div`flex items-center gap-x-1`,
   };
-  const { Layout, Flex } = NavbarTopStyles;
+  const { Layout, Flex, Email, Location } = NavbarTopStyles;
   return (
     <>
       <PageWrapper>
-        <Layout>
+        <Layout className="w-full">
           <Flex>
-            <Paragraph className="text-[#ffc107] whitespace-nowrap">
-              {" "}
-              Opening Hour:
-            </Paragraph>
-            <Paragraph className="whitespace-nowrap">
-              9:00 am to 10:00 pm
-            </Paragraph>
+            <Paragraph className="text-[#ffc107]">Opening Hour:</Paragraph>
+            <Paragraph>9:00 am to 10:00 pm</Paragraph>
           </Flex>
-          <Flex className="gap-x-12 hidden">
-            <Flex>
+          <Flex className="md:hidden">
+            <Email>
+              <HiOutlineMail color="#ffc107" />
+              <Paragraph className="text-xs">info@example</Paragraph>
+            </Email>
+            <Location>
+              <HiOutlineLocationMarker className="text-[#ffc107]" />
+              <Paragraph className="text-xs">
+                Road-01, Block-B, West London City
+              </Paragraph>
+            </Location>
+          </Flex>
+          <Flex className="hidden md:flex gap-x-12">
+            <Email>
               <HiOutlineMail color="#ffc107" />
               <Paragraph>info@example</Paragraph>
-            </Flex>
-            <Flex>
+            </Email>
+            <Location>
               <HiOutlineLocationMarker className="text-[#ffc107]" />
               <Paragraph>Road-01, Block-B, West London City</Paragraph>
-            </Flex>
+            </Location>
           </Flex>
         </Layout>
       </PageWrapper>
